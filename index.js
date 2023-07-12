7e733878098e2191d3fa9e59a84cd5a1e67a5a2c
Ego Maragustaf <egomaragustaf@gmail.com>
2023-07-12 22:51:16 +0700

    feat: implement async await to fetch weather api

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const apiKey = "658b8b7b6d5e6d062f321705ff457f3e";
 const apiURL =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=jakarta";
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function getWeatherData() {
-  const response = await fetch(apiURL + `&appid=${apiKey}`);
-  const data = await response.json();
+const searchCityFormElement = document.getElementById("search-form");
+const searchCityTextElement = document.getElementById("city-text");
 
-  console.log(data);
+async function getWeatherData(city) {
+  try {
+    const response = await fetch(apiURL + city + `&appid=${apiKey}`);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-getWeatherData();
+searchCityFormElement.addEventListener("submit", submitCity);
+
+function submitCity(event) {
+  event.preventDefault();
+
+  getWeatherData(searchCityTextElement.value); //
+}
